df0399831481deb4316d7a4ac66a1f0ccb656731
NooooooooName <dodsi216@naver.com>
2019-06-11 12:29:13 +0900

    ㄴㅇㅀㄴㅇㅀㄴㅇㄹ홍ㄹ호ㅜ

--- WinProj/CMainGame.cpp
+++ WinProj/CMainGame.cpp
@@ -27,14 +27,16 @@ void CMainGame::Init()
 	IMAGE.AddTexture("ExitButton1", "./resource/Button/e_1.bmp");
 	IMAGE.AddTexture("ExitButton2", "./resource/Button/e_2.bmp");
 	IMAGE.AddTexture("ExitButton3", "./resource/Button/e_3.bmp");
+	IMAGE.AddTexture("SoundButton1", "./resource/Button/s_1.bmp");
+	IMAGE.AddTexture("SoundButton2", "./resource/Button/s_2.bmp");
+	IMAGE.AddTexture("SoundButton3", "./resource/Button/s_3.bmp");
 	IMAGE.AddTexture("CloseButton1", "./resource/Button/Close_1.bmp");
 	IMAGE.AddTexture("CloseButton2", "./resource/Button/Close_2.bmp");
 	IMAGE.AddTexture("CloseButton3", "./resource/Button/Close_3.bmp");
 	IMAGE.AddTexture("CreditWindow", "./resource/Credit/CreditWindow.bmp");
-
+	
 	//ChangeRoonScene
-	IMAGE.AddTexture("CRBG", "./resource/Bg/TestBG.bmp");
-
+	IMAGE.AddTexture("CRBG", "./resource/Bg/Test.bmp");
 
 	SCENE.AddScene("TitleScene", new CTitleScene()); // ¾À Ãß°¡
 	SCENE.AddScene("TestScene", new CTestScene());
