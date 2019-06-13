64a01e236eb43f8ed2b6402d38cf00ed175dc021
방저녁 <hyeok020514@naver.com>
2019-06-14 08:39:06 +0900

    일단 배치할 버튼들 넣음

--- WinProj/ChangeRoomScene.cpp
+++ WinProj/ChangeRoomScene.cpp
@@ -4,6 +4,12 @@
 
 ChangeRoomScene::ChangeRoomScene()
 {
+	m_Buttons[BUTTON_PART_BODY] = new CButton();
+	m_Buttons[BUTTON_PART_HAIR] = new CButton();
+	m_Buttons[BUTTON_PART_CLOTHE] = new CButton();
+	m_Buttons[BUTTON_PART_BODY]->Init(IMAGE["PartButton1"], IMAGE["PartButton2"], IMAGE["PartButton3"], {30,681});
+	m_Buttons[BUTTON_PART_HAIR]->Init(IMAGE["PartButton1"], IMAGE["PartButton2"], IMAGE["PartButton3"], {90,681});
+	m_Buttons[BUTTON_PART_CLOTHE]->Init(IMAGE["PartButton1"], IMAGE["PartButton2"], IMAGE["PartButton3"], {150,681});
 }
 
 
@@ -13,12 +19,28 @@ ChangeRoomScene::~ChangeRoomScene()
 
 void ChangeRoomScene::Init()
 {
-
+	
 }
 
 void ChangeRoomScene::Update()
 {
+	for (auto& iter : m_Buttons)
+	{
+		iter.second->Update();
+	}
 
+	if (m_Buttons[BUTTON_PART_BODY]->IsClicked())
+	{
+
+	}
+	if (m_Buttons[BUTTON_PART_HAIR]->IsClicked())
+	{
+
+	}
+	if (m_Buttons[BUTTON_PART_CLOTHE]->IsClicked())
+	{
+
+	}
 }
 
 void ChangeRoomScene::Render()
@@ -26,6 +48,11 @@ void ChangeRoomScene::Render()
 	IMAGE["CRBG"]->Render({ 0,0 }, RGB(0, 0, 0));
 	IMAGE["PartBG"]->Render({ 0,671 }, RGB(1, 0, 0));
 	IMAGE["WearBG"]->Render({ 0,721 }, RGB(1, 0, 0));
+
+	m_Buttons[BUTTON_PART_BODY]->Render(RGB(255, 0, 255));
+	m_Buttons[BUTTON_PART_HAIR]->Render(RGB(255, 0, 255));
+	m_Buttons[BUTTON_PART_CLOTHE]->Render(RGB(255, 0, 255));
+	
 }
 
 void ChangeRoomScene::Release()
